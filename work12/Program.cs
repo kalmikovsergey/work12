7008c6d5375027b336d54afcb233a45c9c88d8fb
Serg <kalmikovw13@gmail.com>
2021-08-20 13:03:49 -0500

    Initial commit
    proba1

---
+++ work12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace work12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
